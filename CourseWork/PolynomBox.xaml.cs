e650fa4effa5a8ebcfc64236221f9b0d37c61ddb
Hluushchenko <Hluushchenko@gmail.com>
2021-05-31 20:55:31 +0300

    Polynomial input added

---
+++ CourseWork/PolynomBox.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+
+
+namespace CourseWork
+{
+    public partial class PolynomBox : UserControl
+    {
+        private List<TextBox> _fields;
+
+        public int Count { get; set; }
+
+        public PolynomBox()
+        {
+            InitializeComponent();
+        }
+
+        public void Show()
+        {
+            _fields = new List<TextBox>();
+            for (int i = 1; i <= Count; i++)
+            {
+                StackPanel stackPanel = new StackPanel {Orientation = Orientation.Horizontal};
+                var field = new TextBox {Width = 30, Height = 20, TextAlignment = TextAlignment.Right};
+                _fields.Add(field);
+                stackPanel.Children.Add(field);
+                if (i != Count)
+                {
+                    TextBlock textBlock = PowerGenerator(Count - i);
+                    stackPanel.Children.Add(textBlock);
+                }
+
+                WrapPanel.Children.Add(stackPanel);
+            }
+        }
+
+        private static TextBlock PowerGenerator(int i)
+        {
+            TextBlock output = new TextBlock {FontSize = 14};
+            output.Text += "x";
+
+            output.Typography.Variants = FontVariants.Superscript;
+            if (i > 1) output.Text += $"{i}";
+            else output.Text += $" ";
+            output.Text += " + ";
+            return output;
+        }
+
+        public Polynomial Value {
+            get
+            {
+                List<double> polynomial = new List<double>();
+                foreach (var field in _fields)
+                {
+                    string currString = field.Text.Replace('.', ',');
+                    double curr = 0;
+                    if (double.TryParse(currString, out curr) )
+                    {
+                        polynomial.Add(curr);
+                    }
+                    else
+                    {
+                        polynomial.Add(0);
+                    }
+                }
+                return new Polynomial(polynomial.ToArray());
+            }
+        }
+    }
+}
---
+++ CourseWork/Polynomial.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace CourseWork
+{
+    public class Polynomial
+    {
+        private double[] _coefficient;
+
+        public Polynomial(params double[] coefficient)
+        {
+            _coefficient = coefficient;
+        }
+        
+
+        public double Value(double x)
+        {
+            double value = 0;
+            for (int i = 0; i < _coefficient.Length; i++)
+            {
+                value += _coefficient[^(i+1)] * Math.Pow(x, i);
+            }
+            return value;
+        }
+
+        public Polynomial Derivative()
+        {
+            double[] newCoefficient = new double[_coefficient.Length - 1];
+            for (int i = 0; i < _coefficient.Length - 1; i++)
+            {
+                newCoefficient[i] = _coefficient[i] * (newCoefficient.Length - i);
+            }
+            return new Polynomial(newCoefficient);
+        }
+
+        public override string ToString()
+        {
+            string output = string.Empty;
+            for (int i = 0; i < _coefficient.Length-1; i++)
+            {
+                output += $"{_coefficient[i]} x^{_coefficient.Length - 1 - i} + ";
+            }
+            output += $"{_coefficient[^1]}";
+            return output;
+        }
+    }
+}
